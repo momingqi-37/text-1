caa192c24f8258053bd1b5bc7a9f9e0668701ee2
momingqi-37 <1271494881@qq.com>
2021-06-02 20:05:47 +0800

    tuser and 0

--- routes/login.js
+++ routes/login.js
@@ -1,48 +1,53 @@
 var express = require('express');
 var router = express.Router();
-var crypto=require('crypto');
-var mysql =require('mysql');
+var crypto = require('crypto');
+var mysql = require('mysql');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('login');
 });
 
-router.post('/login', (req, res) =>{
-// if(req.session.uname ==req.body.uname && req.session.password ==req.body.password){
-//   res.render('1.html');
-// }else{
-//   res.json('没有账号或密码错误，登陆失败')
-// } 
-var connection = mysql.createConnection({
-    host:'localhost',//主机地址
-    user:'root',
-    port:'3306',
-    password:'1234',
-    database:'yyf',
+router.post('/login', (req, res) => {
+  // if(req.session.uname ==req.body.uname && req.session.password ==req.body.password){
+  //   res.render('1.html');
+  // }else{
+  //   res.json('没有账号或密码错误，登陆失败')
+  // } 
+  var connection = mysql.createConnection({
+    host: 'localhost',//主机地址
+    user: 'root',
+    port: '3306',
+    password: '1234',
+    database: 'yyf',
+  });
+
+  connection.connect();
+  let md5 = crypto.createHash("md5");
+  let newPas = md5.update(req.body.password).digest("hex");//加密
+  var mysqlQuery = "select * from tab_creat where name='" + req.body.uname + "'and possword ='" + newPas + "'";
+  connection.query(mysqlQuery, (err, rows) => {
+
+    if (err) {
+      console.log('[query]-:' + err);
+    }
+    else {
+      console.log(rows);
+      if (rows.length == 0) {
+        res.json('账号或密码错误，登陆失败')
+      }
+      else {
+        if (rows[0].type == '0') {
+          res.redirect('/new5');
+        }
+        else {
+          res.render('1.html');
+        }
+      }
+    }
+
+  }); connection.end();
 });
 
-connection.connect();
- let md5 = crypto.createHash("md5");
-let newPas = md5.update(req.body.password).digest("hex");//加密
-var mysqlQuery ="select name,possword from tab_creat where name='"+req.body.uname+"'and possword ='"+newPas+"'";
-connection.query(mysqlQuery,(err,rows)=>{
- 
-  if(err){
-    console.log('[query]-:'+err);
-    }
-  else{
-    console.log(rows);
-      if(rows.length==0){
-    res.json('账号或密码错误，登陆失败')
-     }
-   else{
-       res.render('1.html');
-     }
-    }
-   
-  });connection.end();
-});
-  
 
- 
+
 module.exports = router;
